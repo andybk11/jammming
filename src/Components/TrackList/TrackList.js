1a3b8863ed06a6bb27a94a7e579b27f248e67ce7
Andy <andy@Andys-MacBook-Pro.local>
2018-05-23 14:14:40 -0400

    Add 6 Components

---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './TrackList.css';
+import Track from '../Track/Track';
+
+class TrackList extends React.Component {
+    
+    render() {
+        return(
+         <div class="TrackList">
+    //You will add a map method that renders a set of Track components  -->
+         </div>
+        );
+    }
+}
+
+export default TrackList;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
